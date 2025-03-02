a6b9cae8751709816792ad469c1720ce2325cb13
GabrielDev <gabrys.wisniewski@op.pl>
2025-03-02 18:01:02 +0100

    add: setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="easycnn",
-    version="0.1",
+    version="1.0.0",
     description="Easy tool to create own cnn in 2 minutes with minmum code and a lot of functions.",
     packages=find_packages(),
     author="Gabriel Wiśniewski",
     author_email="gabrys.wisniewski@op.pl",
+    classifiers=[  
+        "Programming Language :: Python :: 3",  
+        "License :: MIT License",  
+        "Operating System :: OS Independent :: Windows", 
+    ],
+     install_requires=[
+        "tensorflow",
+        "numpy",
+        "matplotlib"
+    ],
     
 )
